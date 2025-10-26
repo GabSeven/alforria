8511d726f36c9688f56fd1a1e2c387f027a05b50
GabSeven <gabrodrigomes@gmail.com>
2025-10-26 15:42:11 -0300

    1a visao front

---
+++ backend/app/processors/tsv_processor.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+import pandas as pd
+import io
+
+class Turma(BaseModel):
+    codigo_disciplina: str
+    nome_disciplina: str
+    numero_turma: int
+    curso: str
+
+class Professor(BaseModel):
+    matricula: str
+    nome: str
+    turmas: list[Turma]
+
+
+def tsv_para_estruturado(arquivo: str) -> dict:
+
+    df = pd.read_csv(io.StringIO(arquivo), sep='\t', dtype=str)
+
+    df["curso"] = df["curso"].fillna("")
+
+    colunas_para_string = ['matricula', 'codigo_turma', 'turma', 'carga_horaria']
+    for coluna in colunas_para_string:
+        df[coluna] = df[coluna].astype(str)
+        df[coluna] = df[coluna].str.replace(r'\.0$', '', regex=True)
+   
+    professores= {}
+    for _, linha in df.iterrows():
+        matricula = linha["matricula"]
+        if matricula not in professores:
+            professores[matricula] = {
+                "nome": linha["nome_professor"],
+                "turmas": []
+            }
+        turma_data = Turma(
+            codigo_disciplina=linha["codigo_turma"],
+            nome_disciplina= linha["nome_disciplina"],
+            numero_turma= linha["turma"],
+            curso= linha["curso"]
+        )
+        
+        professores[matricula]["turmas"].append(turma_data)
+        print(professores)
+    return professores
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,62 +1,72 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const API_URL = import.meta.env.API_URL || 'http://127.0.0.1:8000'
 
-function App() {
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const [dados, setDados] = useState<JSON | null>(null);
+interface Professor {
+  matricula: string;
+  nome: string;
+  turmas: Turma[];
+}
 
-  
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files?.[0]) {
-      setArquivo(event.target.files[0]);
-    }
-  };
+interface Turma {
+  codigo_disciplina: string;
+  nome_disciplina: string;
+  numero_turma: number;
+  curso: string;
+}
+
+
+function App() {
+  const [dados, setDados] = useState<Professor[] | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
+    
+    const arquivo = fileInputRef.current?.files?.[0];
     if (!arquivo) return;
+    
+    const formData = new FormData();
+    formData.append('arquivo', arquivo);
+    
     const response = await fetch(`${API_URL}/professores`, {
       method: 'POST',
-      body: arquivo
+      body: formData
     });
-    const dados = await response.json()
-    setDados(dados);
-  };
-  
-  const handleButton = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const response = await fetch(`${API_URL}/professores`, {
-      method: 'GET'
-    });
-    const dados = await response.json()
+    
+    const dados = await response.json();
     setDados(dados);
   };
 
+  const professores = dados ? Object.values(dados) : [];
+  
   return (
     <>
-      <form onSubmit={handleSubmit} style={{
-        display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
-        }}>
+      <form onSubmit={handleSubmit}>
         <input 
           type="file" 
           accept=".tsv" 
-          ref={fileInputRef} 
+          ref={fileInputRef}
         />
         <button type="submit">Enviar arquivo</button>
       </form>
-      <button onClick={handleButton}> aa</button>
-      {!dados && (
-        <>calma</>
-      )} 
-      {dados && (
-        <>oi</>
-      )} 
+      123
+      <div>
+      {professores.map(professor => {
+        return(
+          <div className="tabela-professor" key={professor.matricula}> {professor.nome}
+          {professor.turmas.map(turma => {
+            // return(<div className='disciplina'>{JSON.stringify(turma)}</div>)
+            return(<div className='disciplina'>{`${turma.codigo_disciplina} - ${turma?.nome_disciplina}`}</div>)
+          })}
+          </div>
+          
+        )
+      })}
+      </div>
     </>
   );
 }
